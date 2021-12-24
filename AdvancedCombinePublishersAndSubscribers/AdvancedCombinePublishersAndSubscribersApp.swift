1a381608f790e82e54d4900e74ecc3c8db7ad473
Frederick Javalera <frederickjavalera@gmail.com>
2021-12-23 17:52:33 -0700

    Initial Commit

---
+++ AdvancedCombinePublishersAndSubscribers/AdvancedCombinePublishersAndSubscribersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedCombinePublishersAndSubscribersApp.swift
+//  AdvancedCombinePublishersAndSubscribers
+//
+//  Created by Fred Javalera on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedCombinePublishersAndSubscribersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
